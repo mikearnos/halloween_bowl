da81d5498ecbcf4359d25aceb48b82995f103c2b
mikearnos <mikearnos@gmail.com>
2022-10-05 02:38:23 -0400

    created hardware.h

---
+++ include/hardware.h
@@ -0,0 +1,12 @@
+#define IRSENSOR PD2
+#define BATTIN A0
+
+const float R1 = 99400;
+const float R2 = 5520;
+const float Vref = 1.135; // fine tune the so 7.2 battery voltage matches
+
+#define BATTERY_COUNT 2
+#define BATTERY_MAX 4.2
+#define BATTERY_MIN 3.0
+#define VOLTAGE_MAX BATTERY_MAX * BATTERY_COUNT
+#define VOLTAGE_MIN BATTERY_MIN * BATTERY_COUNT
