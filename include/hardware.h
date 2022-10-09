bf9aa4769a28955fee1bc721ff935534899bd24f
mikearnos <mikearnos@gmail.com>
2022-10-08 22:58:49 -0400

    parentheses just in case

--- include/hardware.h
+++ include/hardware.h
@@ -8,5 +8,5 @@ const float Vref = 1.135; // fine tune the so 7.2 battery voltage matches
 #define BATTERY_COUNT 2
 #define BATTERY_MAX 4.2
 #define BATTERY_MIN 3.0
-#define VOLTAGE_MAX BATTERY_MAX * BATTERY_COUNT
-#define VOLTAGE_MIN BATTERY_MIN * BATTERY_COUNT
+#define VOLTAGE_MAX (BATTERY_MAX * BATTERY_COUNT)
+#define VOLTAGE_MIN (BATTERY_MIN * BATTERY_COUNT)
