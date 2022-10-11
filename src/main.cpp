efb2ec442fe53ed39ace584149dfc0dfae2fc55c
mikearnos <mikearnos@gmail.com>
2022-10-10 20:12:28 -0400

    added some servo and DF1201S stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <arduino.h>
 #include "pacifica.h"
 #include "hardware.h"
+#include <Servo.h>
+#include <DFRobot_DF1201S.h>
+#include <SoftwareSerial.h>
 
 bool nonBlockDelay(unsigned long* last, unsigned int delay);
 float voltageMeasurement(void);
 void batteryLoop(void);
+void dfplayerSetup(void);
+
+SoftwareSerial DF1201SSerial(DFRX, DFTX); //RX  TX
+DFRobot_DF1201S DF1201S;
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
 
 void setup()
 {
@@ -13,27 +24,66 @@ void setup()
     pinMode(BATTIN, INPUT);
     analogReference(INTERNAL); // 1.1 volts
 
-    Serial.begin(115200);
+    //servo1.attach(9);
+    //servo2.attach(10);
+    //servo3.attach(11);
+
     pacificaSetup();
+
+    Serial.begin(115200);
+
+    dfplayerSetup();
+}
+
+void dfplayerSetup(void)
+{
+    DF1201SSerial.begin(9600);
+    while (!DF1201S.begin(DF1201SSerial)) {
+        Serial.print("Init failed, please check the wire connection! ");
+        Serial.println(millis());
+        delay(10);
+    }
+
+    // need to power cycle module after these are changed
+    //DF1201S.setBaudRate(9600);
+    //DF1201A.setLED(off)
+    //DF1201S.setPrompt(false);
+
+    DF1201S.setVol(20);
+    Serial.print("VOL:");
+    Serial.println(DF1201S.getVol());
+
+    DF1201S.switchFunction(DF1201S.MUSIC);
+    DF1201S.setPlayMode(DF1201S.SINGLE);
+
+    Serial.print("PlayMode:");
+    Serial.println(DF1201S.getPlayMode());
 }
 
 unsigned long lastBlink = 0;
+unsigned long lastPlayer = 0;
 
 void loop()
 {
-    static unsigned long lastHand;
+    //delay(1000);
+    //DF1201S.setVol(/*VOL = */ 10);
+    if (nonBlockDelay(&lastPlayer, 10000)) {
+        DF1201S.playFileNum(SND_BARREL);
+    }
+    //static unsigned long lastHand;
     int mode = GREEN;
 
-    if (!digitalRead(IRSENSOR)) {
+    /*if (!digitalRead(IRSENSOR)) {
         if (nonBlockDelay(&lastHand, 5000)) {
             Serial.print("hand detected ");
             Serial.println(millis());
             //mode = REDFLASH;
         }
-    }
+    }*/
 
+    //servoLoop();
     pacificaLoop(mode);
-    batteryLoop();
+    //batteryLoop();
 }
 
 void batteryLoop(void)
@@ -58,6 +108,33 @@ void batteryLoop(void)
             fastLEDLowBattery();
         }
     }
+}
+
+void servoLoop(void)
+{
+    //static unsigned long lastServo;
+    static int direction, i;
+    //    if (nonBlockDelay(&lastServo, 10)) {
+    if (direction == 0) {
+        servo1.write(i);
+        servo2.write(i);
+        servo3.write(i);
+        if (i == 150) {
+            direction = 1;
+        } else {
+            i++;
+        }
+    } else {
+        servo1.write(i);
+        servo2.write(i);
+        servo3.write(i);
+        if (i == 0) {
+            direction = 0;
+        } else {
+            i--;
+        }
+    }
+    //}
 }
 
 bool nonBlockDelay(unsigned long* last, unsigned int delay)
