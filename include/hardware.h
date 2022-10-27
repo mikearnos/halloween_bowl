293ea47fee528a35672bceaac92d637b4f1e8b2a
mikearnos <mikearnos@gmail.com>
2022-10-27 11:02:08 -0400

    added sound values
    changed resistor for voltage divider

--- include/hardware.h
+++ include/hardware.h
@@ -1,14 +1,18 @@
+#define DFRX 2
+#define DFTX 3
+//#define DATA_PIN 4  in pacifica.cpp
 #define IRSENSOR PD5
 #define BATTIN A1
 
 #define SND_BARREL 1
 #define SND_PIPE 2
-
-#define DFTX 3
-#define DFRX 2
+#define SND_CAPCOM 3
+#define SND_DONT 4
+#define SND_TURN_OFF 5
+#define SND_MY_MAN 6
 
 const float R1 = 99400;
-const float R2 = 5520;
+const float R2 = 14720;
 const float Vref = 1.135; // fine tune the so 7.2 battery voltage matches
 
 #define BATTERY_COUNT 2
