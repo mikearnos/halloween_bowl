42487d8d80d5eb46d956a6d8ec66e2f55b7d2fed
mikearnos <mikearnos@gmail.com>
2022-10-27 15:52:15 -0400

    changed the comment

--- include/hardware.h
+++ include/hardware.h
@@ -14,7 +14,7 @@
 
 const float R1 = 99400;
 const float R2 = 14720;
-const float Vref = 1.135; // fine tune the so 7.2 battery voltage matches
+const float Vref = 1.135; // the voltage measured on AREF
 
 #define BATTERY_COUNT 2
 #define BATTERY_MAX 4.2
