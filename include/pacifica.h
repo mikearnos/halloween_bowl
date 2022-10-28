fd9d22b37cd682bce5dc532a087d8d794aa73ebf
mikearnos <mikearnos@gmail.com>
2022-10-28 17:40:18 -0400

    added two flash prototypes

--- include/pacifica.h
+++ include/pacifica.h
@@ -1,6 +1,9 @@
 #define GREEN 0
 #define REDFLASH 1
 
+void redFlash(uint8_t);
+void purpleFlash(uint8_t);
+
 extern void fastLEDLowBattery(void);
 extern void pacificaSetup(void);
 extern void pacificaLoop(int);
