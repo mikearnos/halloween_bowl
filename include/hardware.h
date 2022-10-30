9b82035fe66bd5fddb6534144511d4f7c0d1a327
mikearnos <mikearnos@gmail.com>
2022-10-29 23:27:10 -0400

    added COMMS pins

--- include/hardware.h
+++ include/hardware.h
@@ -1,9 +1,12 @@
 #define BATTIN A0
-#define TRIGGER PD2
-//#define TRIGGER2 PD3 reserved
+#define COMMS1 PD3 // pink
+#define COMMS2 PD2 // purple
 //#define DATA_PIN 4  in pacifica.cpp
 #define DFRX PD5
 #define DFTX PD6
+
+#define TRIGGER COMMS1
+#define ARMED COMMS2
 
 #define SND_BARREL 1
 #define SND_PIPE 2
@@ -15,7 +18,7 @@
 const float R1 = 99400;
 const float R2 = 14720;
 //const float Vref = 1.135; // the voltage measured on AREF
-const float Vref = 1.095; // the voltage measured on AREF
+const float Vref = 1.05; // the voltage measured on AREF
 
 #define BATTERY_COUNT 2
 #define BATTERY_MAX 4.2
